84a68af4f0852406c3e91c83b52ed2297157c451
N E M O <nemoxhan@gmail.com>
2021-07-01 07:17:23 -0700

    there was a typo in path of index.js

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ openNav.onclick = (e) => {
 closeNav.onclick = (e) => {
   nav.style.width = "0";
 };
+
+const navBtns = nav.querySelectorAll("a");
+navBtns.forEach((btn) => {
+  btn.onclick = (e) => {
+    nav.style.width = "0";
+  };
+});
